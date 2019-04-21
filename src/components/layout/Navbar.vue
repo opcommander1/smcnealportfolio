d7f752184d50b6c8c700a23b5385122e7204819b
Shereal McNeal <opcommander1@hotmail.com>
2019-04-21 10:19:24 -0500

    update mobile resp sidenav

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,7 +2,8 @@
  <div class="navbar">
    <nav class="black">
      <div class="container">
-       <ul class="left">
+       <a href="#" data-target="mobile-resp" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+       <ul class="left hide-on-med-and-down">
          <li><a href="#about">About</a></li>
          <li><a href="#skills">Skills</a></li>
          <li><a href="#projects">Projects</a></li>
@@ -10,6 +11,12 @@
        </ul>
      </div>
    </nav>
+      <ul class="sidenav" id="mobile-resp">
+            <li><a class="sidenav-close" href="#about">About</a></li>
+            <li><a class="sidenav-close" href="#skills">Skills</a></li>
+            <li><a class="sidenav-close" href="#projects">Projects</a></li>
+            <li><a class="sidenav-close" href="#contact">Contact</a></li>
+      </ul>
  </div>
 </template>
 
@@ -22,12 +29,13 @@ export default {
     }
   }
 }
+
 </script>
 
 <style>
 
 .navbar li a{
-font-size: 3.1vw;
+font-size: 20px;
 font-weight: 300;
 transition: color .3s ease-in-out;
 }
